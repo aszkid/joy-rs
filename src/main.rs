23c5609dbaa2fdd62924035c3fa2ccdde013cb28
Pol Gómez Riquelme <polgomezriquelme@gmail.com>
2019-11-29 17:59:46 -0600

    Definitions!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 enum StackElem {
@@ -7,39 +8,51 @@ enum StackElem {
     Quotation(Vec<String>),
 }
 
-fn exec_raw(input: &String, mut stack: &mut Vec<StackElem>) -> bool {
+fn exec_raw(input: &String, mut stack: &mut Vec<StackElem>, mut programs: &mut HashMap<String, Vec<String>>) -> bool {
     let mut vec = input
         .trim().split(" ")
         .map(String::from)
         .collect::<Vec<_>>();
-    exec(&mut vec, &mut stack)
+    exec(&mut vec, &mut stack, &mut programs)
 }
 
-fn exec(vec: &mut Vec<String>, mut stack: &mut Vec<StackElem>) -> bool {
+fn exec(vec: &mut Vec<String>, mut stack: &mut Vec<StackElem>, mut programs: &mut HashMap<String, Vec<String>>) -> bool {
     let mut quit = false;
-    let mut program = Vec::new();
-    let mut in_quotation = false;
-    vec.reverse();
 
+    let mut in_quotation = false;
+    let mut quotation = Vec::new();
+
+    let mut in_definition = false;
+    let mut definition = Vec::new();
+    let mut definition_name = String::from("");
+
+    vec.reverse();
     while !vec.is_empty() {
         let tok = vec.pop().unwrap();
 
         if in_quotation {
             if tok == "]" {
-                stack.push(StackElem::Quotation(program));
-                program = Vec::new();
+                stack.push(StackElem::Quotation(quotation));
+                quotation = Vec::new();
                 in_quotation = false;
                 continue;
             } else {
-                program.push(tok);
+                quotation.push(tok);
                 continue;
             }
+        }
+        if in_definition {
+            if tok == "==" {
+                continue;
+            }
+            definition.push(tok);
+            continue;
         }
 
         match tok.as_ref() {
             "[" => {
                 in_quotation = true;
-                program.clear();
+                quotation.clear();
             },
             "+" => {
                 let a = match stack.pop().unwrap() {
@@ -96,6 +109,20 @@ fn exec(vec: &mut Vec<String>, mut stack: &mut Vec<StackElem>) -> bool {
                 };
                 stack.push(StackElem::Boolean(a < b));
             },
+            "=" => {
+                let b = match stack.pop().unwrap() {
+                    StackElem::Number(num) => num,
+                    _ => panic!("`=` expects two numbers")
+                };
+                let a = match stack.pop().unwrap() {
+                    StackElem::Number(num) => num,
+                    _ => panic!("`=` expects two numbers")
+                };
+                stack.push(StackElem::Boolean(a == b));
+            },
+            "pop" => {
+                stack.pop().unwrap();
+            },
             "dup" => {
                 let x = stack.pop().unwrap();
                 stack.push(x.clone());
@@ -133,14 +160,14 @@ fn exec(vec: &mut Vec<String>, mut stack: &mut Vec<StackElem>) -> bool {
                 for v in original {
                     let mut sub = vec![v.clone()];
                     sub.extend(filter.clone());
-                    quit = exec(&mut sub, &mut stack);
+                    quit = exec(&mut sub, &mut stack, &mut programs);
                     match stack.pop().unwrap() {
                         StackElem::Boolean(b) => {
                             if b {
                                 result.push(v);
                             }
                         },
-                        _ => panic!("`filter` predicate must return a boolean")
+                        _ => panic!("`filter` predicate must push a boolean")
                     }
                 }
                 stack.push(StackElem::Quotation(result));
@@ -163,7 +190,32 @@ fn exec(vec: &mut Vec<String>, mut stack: &mut Vec<StackElem>) -> bool {
                     sub.push(v);
                     sub.extend(pred.clone());
                 }
-                quit = exec(&mut sub, &mut stack);
+                quit = exec(&mut sub, &mut stack, &mut programs);
+            },
+            "ifte" => {
+                let mut else_pred = match stack.pop().unwrap() {
+                    StackElem::Quotation(q) => q,
+                    _ => panic!("`ifte` expects an else predicate")
+                };
+                let mut then_pred = match stack.pop().unwrap() {
+                    StackElem::Quotation(q) => q,
+                    _ => panic!("`ifte` expects a then predicate")
+                };
+                let mut if_pred = match stack.pop().unwrap() {
+                    StackElem::Quotation(q) => q,
+                    _ => panic!("`ifte` expects an if predicate")
+                };
+                let mut stack_cloned = stack.clone();
+                quit = exec(&mut if_pred, &mut stack_cloned, &mut programs);
+                let if_result = match stack_cloned.pop().unwrap() {
+                    StackElem::Boolean(b) => b,
+                    _ => panic!("`ifte` if predicate must push a boolean")
+                };
+                if if_result {
+                    quit = exec(&mut then_pred, &mut stack, &mut programs);
+                } else {
+                    quit = exec(&mut else_pred, &mut stack, &mut programs);
+                }
             },
             "quit" => {
                 quit = true;
@@ -171,9 +223,27 @@ fn exec(vec: &mut Vec<String>, mut stack: &mut Vec<StackElem>) -> bool {
             },
             _ => match tok.parse::<i32>() {
                 Ok(num) => stack.push(StackElem::Number(num)),
-                Err(_) => println!("unknown token: {}", tok),
+                Err(_) => {
+                    match programs.get(&tok) {
+                        Some(p) => {
+                            quit = exec(&mut p.clone(), &mut stack, &mut programs);
+                        },
+                        None => {
+                            if tok == definition_name {
+                                continue;
+                            }
+                            println!("defining a program `{}`", tok);
+                            definition_name = tok;
+                            in_definition = true;
+                        }
+                    }
+                },
             },
         }
+    }
+
+    if in_definition {
+        programs.insert(definition_name, definition);
     }
 
     quit
@@ -185,6 +255,12 @@ fn main() {
     let mut stack: Vec<StackElem> = Vec::new();
     let mut quit = false;
 
+    let mut programs = HashMap::new();
+    programs.insert(
+        "square".to_string(),
+        vec!["dup", "*"].iter().map(|x| x.to_string()).collect::<Vec<_>>()
+    );
+
     while !quit {
         print!("> ");
         io::stdout().flush().unwrap();
@@ -194,7 +270,7 @@ fn main() {
             _ => {},
         }
 
-        quit = exec_raw(&input, &mut stack);
+        quit = exec_raw(&input, &mut stack, &mut programs);
         println!("{:?}", stack);
     }
 
